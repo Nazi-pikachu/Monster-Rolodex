ddad2eefd46c38b7baed7e23c84ab5cdbffa2f81
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-08-15 23:45:38 +0530

    Updates

---
+++ precache-manifest.096389a6e74bed9e9cb1641eb9768cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6e834bfe6bc67907d1e37a479b69d4",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "84f4b766c40ca9edca7b",
+    "url": "/Monster-Rolodex/static/css/main.a3277812.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Monster-Rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f4b766c40ca9edca7b",
+    "url": "/Monster-Rolodex/static/js/main.842ca2e4.chunk.js"
+  },
+  {
+    "revision": "3ca3d2aacf82f3812c9e",
+    "url": "/Monster-Rolodex/static/js/runtime-main.c58f4534.js"
+  }
+]);
